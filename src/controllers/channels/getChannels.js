3c042ab18053ac82cd9ac3528dddbdeea5101ca2
Rushnett <rushnett@gmail.com>
2021-08-22 03:47:35 +0000

    update(channels): add a filter query for getChannels

--- src/controllers/channels/getChannels.js
+++ src/controllers/channels/getChannels.js
@@ -1,8 +1,15 @@
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 const { checkJWT } = require('../../helpers/checkAuth')
 
 module.exports = async function (req, res) {
     checkJWT(req, res, async () => {
-        const channels = await Channels.findAll()
+        const where = {}
+
+        if (req.query.filter)
+            where.name = { [Op.like]: `${req.query.filter }%`}
+
+        const channels = await Channels.findAll({ where: where })
         res.status(200).json(channels)
     })
 }
