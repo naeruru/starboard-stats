420141742ec65c67fc637f5777fc4fedcc853f5b
Rushnett <rushnett@gmail.com>
2021-08-22 03:42:10 +0000

    fix(channels): uncomment jwt check for timeline data

--- src/controllers/channels/getTimeline.js
+++ src/controllers/channels/getTimeline.js
@@ -3,26 +3,7 @@ const { checkJWT } = require('../../helpers/checkAuth')
 
 module.exports = async function (req, res) {
 
-//    checkJWT(req, res, async () => {
-
-        // // get channels
-        // const channels = await Channels.findAll()
-
-        // // map each channel to a timeline of dates grouped by date
-        // const timelines = await Promise.all(channels.map(async (channel) => {
-        //     const timeline = await Posts.findAll({
-        //         where: {
-        //             channelId: channel.id
-        //         },
-        //         attributes: [
-        //             [sequelize.fn('DATE_FORMAT', sequelize.col('date'), '%Y-%m-%d 00:00:00'), 'day'],
-        //             [sequelize.fn('COUNT', 'date'), 'posts'], 
-        //             [sequelize.fn('sum', sequelize.col('reactions')), 'reactions']
-        //         ],
-        //         group: ['day', 'channelId']
-        //     })
-        //     return {id: channel.id, name: channel.name, timeline }
-        // }))
+   checkJWT(req, res, async () => {
 
         const whereClause = {}
         if (req.query.channel)
@@ -45,6 +26,6 @@ module.exports = async function (req, res) {
         })
 
         res.status(200).json(timeline)
-//    })
+   })
     
 }
